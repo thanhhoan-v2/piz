7789374a8f1c53bb64f22ecd8c8f8f6ce405c292
thanhhoann <pdth456@gmail.com>
2024-09-16 11:01:24 +0700

    db(migrate): add migration

--- atoms/user.ts
+++ atoms/user.ts
@@ -1,44 +1,6 @@
 import type { User } from "@supabase/supabase-js"
 import { atomWithStorage } from "jotai/utils"
 
-type ICustomTheme = {
-	group: "light" | "dark"
-	label: string
-	value: string
-}
-
-export const customThemes: ICustomTheme[] = [
-	{
-		group: "light",
-		label: "Big Squares",
-		value: "light_big_squares",
-	},
-	{
-		group: "light",
-		label: "Small Squares",
-		value: "light_small_squares",
-	},
-	{
-		group: "light",
-		label: "Gradient Violet",
-		value: "light_gradient_violet",
-	},
-	{
-		group: "dark",
-		label: "Gradient Violet",
-		value: "dark_gradient_violet",
-	},
-	{
-		group: "dark",
-		label: "Small Squares",
-		value: "dark_small_squares",
-	},
-]
-
 export const userAtom = atomWithStorage<User | null>("piz_current_user", null)
-export const customThemeAtom = atomWithStorage<ICustomTheme>(
-	"piz_custom_theme",
-	customThemes[0],
-)
 
 userAtom.debugLabel = "user"
