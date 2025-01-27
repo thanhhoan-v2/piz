fd9ccbf30509b884ed3b702f2ac1a6f58061b5db
thanhhoann <pdth456@gmail.com>
2025-01-27 07:13:54 +0700

    fix(profile-page): add props

--- app/(user-profile)/[userName]/page.tsx
+++ app/(user-profile)/[userName]/page.tsx
@@ -1,12 +1,12 @@
 import UserProfile from "@components/ui/profile/UserProfile"
 import { stackServerApp } from "../../../stack"
 
-type Params = Awaited<{ params: { userName: string } }>
+type Params = Promise<{ userName: string }>
 
-export default async function UserPage({ params }: Params) {
-	const user = await stackServerApp.getUser(params.userName)
+export default async function UserPage(props: { params: Params }) {
+	const { userName } = await props.params
+	const user = await stackServerApp.getUser(userName)
 
-	// Serialize the user object by picking only the needed properties
 	const serializedUser = {
 		id: user?.id ?? "",
 		userName: user?.displayName ?? "",
